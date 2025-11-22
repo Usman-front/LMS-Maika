62d269eae062d459dd1d7d8825e421252303bc41
Usman <usman64446@gmail.com>
2025-11-22 23:03:55 +0500

    second commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/smartlearn'
+const uri = process.env.MONGODB_URI || 'mongodb+srv://sadia031755_db_user:1122334455@cluster0.qghapru.mongodb.net/?appName=Cluster0'
 
 export async function connectDB() {
   mongoose.set('strictQuery', true)
